3626d0cae94951bbdabf7a2d6ec3e1dc6ffbe132
ankit jain <ankitjain@mac.local>
2022-12-24 18:33:43 +0530

    useEffect demo

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,5 +1,5 @@
 const BlogList = (props) => {
-    const { blogs, title, handleDelete } = props;
+    const { blogs, title } = props;
     return (
         <div className="blog-list">
             <h2>{title}</h2>
@@ -7,7 +7,6 @@ const BlogList = (props) => {
                 return (<div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
-                    <button onClick={() => handleDelete(blog.id)}>delete blog</button>
                 </div>);
             })}
         </div>
--- src/home.js
+++ src/home.js
@@ -1,19 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
+
+    const [isPending, setIsPending] = useState(true);
+
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then((data) => {
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch((err) => {
+                setError(err);
+                setIsPending(false);
+                console.log(err.message);
+            });
+        }, 1000);
+    }, []);
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            { error && 
+              <div>Error: {error.message}</div>
+            }
+            { isPending && 
+              <div>...Loading</div>
+            }
+            { blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
     );
 }
