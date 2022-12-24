fc3c723d7accb635bedcbe4ac59bb4cbd2837b99
ankit jain <ankitjain@mac.local>
2022-12-24 18:44:42 +0530

    Custom useFetch Hook

--- src/home.js
+++ src/home.js
@@ -1,31 +1,8 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-
-    const [isPending, setIsPending] = useState(true);
-
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then((data) => {
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch((err) => {
-                setError(err);
-                setIsPending(false);
-                console.log(err.message);
-            });
-        }, 1000);
-    }, []);
+    const {data: blogs, isPending, error}  = useFetch('http://localhost:8000/blogs');
 
     return (
         <div className="home">
