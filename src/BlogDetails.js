285ed0d39b4dbc73eefc4c17af5e8afa94b3facc
ankit jain <ankitjain@mac.local>
2022-12-24 20:34:22 +0530

    useHistory redirects demo

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,18 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const {data: blog, isPending, error}  = useFetch(`http://localhost:8000/blogs/${id}`);
+    const history = useHistory();
+    const handleClick = () => {
+        fetch(`http://localhost:8000/blogs/${id}`, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/')
+        })
+    };
     return (  
         <div className="blog-details">
             { isPending && 
@@ -19,6 +28,7 @@ const BlogDetails = () => {
                     <div>
                         {blog.body}
                     </div>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             }
         </div>
