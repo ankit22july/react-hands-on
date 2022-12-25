c0c77de88e4680f30205a61bf3d8bb9812388d10
ankit jain <ankitjain@mac.local>
2022-12-25 08:59:55 +0530

    404 handling demo

---
+++ src/NotFound.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return (  
+        <div className="not-found">
+            <h2>Sorry</h2>
+            <p>That Page cannot be found</p>
+            <Link to></Link>
+        </div>
+    );
+}
+ 
+export default NotFound;
